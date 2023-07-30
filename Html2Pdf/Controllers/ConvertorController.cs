a6de5cbbedd8214d91ed06bfa7a0adbe003e9d6c
Faisal Andrabi <faisalbinsyed@gmail.com>
2023-07-30 21:24:08 +0530

    API Done

---
+++ Html2Pdf/Controllers/ConvertorController.cs
@@ -0,0 +1,30 @@
+﻿using Html2Pdf.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Html2Pdf.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConvertorController : ControllerBase
+    {
+        [HttpGet(Name = "2Pdf")]
+        public IActionResult Get(String html)
+        {
+            if (String.IsNullOrEmpty(html))
+            {
+                return NotFound();
+            }
+
+            PdfConvertor pc = new PdfConvertor();
+            Guid guid = Guid.NewGuid();
+            
+            return File(
+            pc.ToPdf(html),
+            "application/pdf",
+            guid+".pdf"
+                );
+        }
+
+    }
+
+}
---
+++ Html2Pdf/Logic/PdfConvertor.cs
@@ -0,0 +1,23 @@
+﻿
+using SelectPdf;
+using System.Drawing;
+
+namespace Html2Pdf.Logic
+{
+    public class PdfConvertor
+    {
+        public byte[] ToPdf(String html)
+        {
+            html = html.Replace("StrTag", "<").Replace("EndTag", ">");
+            HtmlToPdf h2p = new HtmlToPdf();
+            h2p.Options.EmbedFonts = true;
+            
+            PdfDocument pdfDoc = h2p.ConvertHtmlString(html);
+            PdfFont pf = pdfDoc.AddFont(new System.Drawing.Font("Verdana",15));
+            byte[] pdf = pdfDoc.Save();
+            pdfDoc.Close();
+            return pdf;
+        }
+
+    }
+}
